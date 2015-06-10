343ea1368053fd5b6523ac39792a9ee70b82865a
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-10 18:03:57 +0200

    add class spec

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -12,7 +12,7 @@ describe 'role_deployserver' do
           let(:params) {{ }}
 
           it { is_expected.to compile.with_all_deps }
-       
+          it { is_expected.to contain_class('role_deployserver') }       
           it { is_expected.to contain_class('profile_base') }
           it { is_expected.to contain_class('profile_puppetmaster') }
 
