bf73d619b0592bfe8803cc03dbaa7a80eea735df
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-16 18:21:05 +0200

    fix concat_basedir fact

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -5,11 +5,12 @@ describe 'role_deployserver' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
         let(:facts) do
-          facts
+          facts.merge({
+            :concat_basedir => "/foo"
+          })
         end
 
         context "role_deployserver class without any parameters" do
-          let(:params) {{ }}
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_class('role_deployserver') }       
